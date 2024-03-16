809290fd2e71126591aa82c00ec7c48753b342c1
Zeeshan Ahmad <ahmadzee26@gmail.com>
2024-03-16 12:19:44 +0500

    Update __init__.py

--- exchange_apis/__init__.py
+++ exchange_apis/__init__.py
@@ -1,1 +1,11 @@
+# exchange_apis/__init__.py
+
+"""
+The exchange_apis package contains modules for integrating with various cryptocurrency exchanges.
+"""
+
+# Import specific exchange API modules or classes to expose them
+from .binance_api import BinanceAPI
+
+__all__ = ["BinanceAPI"]
 # Exchange APIs 
