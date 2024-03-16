bb8d3d6249674dba4efbc79cf1b624aedfa3c62b
Zeeshan Ahmad <ahmadzee26@gmail.com>
2024-03-16 12:23:55 +0500

    Update __init__.py

--- utilities/__init__.py
+++ utilities/__init__.py
@@ -1,1 +1,11 @@
+# utilities/__init__.py
+
+"""
+The utilities package contains helper functions and classes for the arbitrage bot.
+"""
+
+# Import utility functions or classes to expose them
+from .logger import setup_logger
+
+__all__ = ["setup_logger"]
 # Utilities Module 
